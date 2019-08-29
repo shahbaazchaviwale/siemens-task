75eb2948ad1e3049555d81e2047cf05dc543f5ff
Shahbaaz <shahbaazchaviwale@gmail.com>
2019-08-29 09:44:19 +0530

    29-08-2019-9:45am

--- siemens-ui/src/app/app.module.ts
+++ siemens-ui/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RoughComponentComponent } from './rough-component/rough-component.component';
 import { MaterialModule } from 'src/material.module';
+import { HeaderSectionComponent } from './header-section/header-section.component';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
+import { StatusCardComponent } from './status-card/status-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoughComponentComponent
+    RoughComponentComponent,
+    HeaderSectionComponent,
+    HeaderMenuComponent,
+    StatusCardComponent
   ],
   imports: [
     BrowserModule,
