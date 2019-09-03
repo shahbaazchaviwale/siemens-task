b89b98abf82b95bfba877c3b717021958842c6a4
CHAVIWALE <shahbaz.chaviwale.ext@siemens.com>
2019-09-03 10:30:49 +0530

    ui-task-added

--- siemens-ui/src/app/slider/slider.component.ts
+++ siemens-ui/src/app/slider/slider.component.ts
@@ -7,16 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderComponent implements OnInit {
 
-  
-  public NextPassNumber: number = 2;
-  public PrePassNumber: number = 1;
-  public nextNumber: number = 1;
+
+  public NextPassNumber = 2;
+  public PrePassNumber = 1;
+  public nextNumber = 1;
   public NextbtnDisable = true;
   public PrebtnDisable = false;
   public data = [
-    { "id": 1, "name": "Alita Oliva", "lastseen": "5", "class": "one-1" },
-    { "id": 2, "name": "Evliyn Salt", "lastseen": "6", "class": "one-2" },
-    { "id": 3, "name": "Maria Dsaouza", "lastseen": "3", "class": "one-3" },
+    { id: 1, name: 'Alita Oliva', lastseen: '5', class: 'one-1' },
+    { id: 2, name: 'Evliyn Salt', lastseen: '6', class: 'one-2' },
+    { id: 3, name: 'Maria Dsaouza', lastseen: '3', class: 'one-3' },
   ];
 
   constructor() { }
@@ -24,49 +24,48 @@ export class SliderComponent implements OnInit {
   ngOnInit() {
   }
 
-  previous(){
-    if(this.PrePassNumber >= this.data.length ) {
+  previous() {
+    if (this.PrePassNumber >= this.data.length) {
       // this.passNumber = 1
-      this.NextbtnDisable = false
+      this.NextbtnDisable = false;
     }
-      this.data.map( (value, index) => {
-        if(index <= this.data.length - 1){
-          if(value.id === this.PrePassNumber){
-            value.class = 'one-1 ani'
-            console.log('data >>');
-          }else{
-            index <= this.data.length - 2 ? value.class = 'one-'+ (this.nextNumber + 1) : value.class = 'one-'+ index + 1; 
-          }
-        }
-          this.nextNumber--
-      });
-      console.log('list >>', this.data);
-      this.PrePassNumber--;
-      this.nextNumber = 1
-  }
-
-  next(){
-
-  if(this.NextPassNumber >= this.data.length ) {
-    // this.passNumber = 1
-    this.NextbtnDisable = false
-  }
-  this.NextbtnDisable = true;
-    this.data.map( (value, index) => {
-      if(index <= this.data.length - 1){
-        if(value.id === this.NextPassNumber){
-          value.class = 'one-1 ani'
+    this.data.map((value, index) => {
+      if (index <= this.data.length - 1) {
+        if (value.id === this.PrePassNumber) {
+          value.class = 'one-1 ani';
           console.log('data >>');
-        }else{
-          index <= this.data.length - 2 ? value.class = 'one-'+ (this.nextNumber + 1) : value.class = 'one-'+ value.id; 
+        } else {
+          index <= this.data.length - 2 ? value.class = 'one-' + (this.nextNumber + 1) : value.class = 'one-' + index + 1;
         }
       }
-        this.nextNumber++
+      this.nextNumber--;
     });
-    console.log('list >>', this.data);
-    this.NextPassNumber++;
-    this.nextNumber = 1
+    this.PrePassNumber--;
+    this.nextNumber = 1;
   }
 
-  
+  // next() {
+
+  //   if (this.NextPassNumber >= this.data.length) {
+  //     // this.passNumber = 1
+  //     this.NextbtnDisable = false;
+  //   }
+  //   this.NextbtnDisable = true;
+  //   this.data.map((value, index) => {
+  //     if (index <= this.data.length - 1) {
+  //       if (value.id === this.NextPassNumber) {
+  //         value.class = 'one-1 ani'
+  //         console.log('data >>');
+  //       } else {
+  //         index <= this.data.length - 2 ? value.class = 'one-' + (this.nextNumber + 1) : value.class = 'one-' + value.id;
+  //       }
+  //     }
+  //     this.nextNumber++;
+  //   });
+  //   console.log('list >>', this.data);
+  //   this.NextPassNumber++;
+  //   this.nextNumber = 1;
+  // }
+
+
 }
